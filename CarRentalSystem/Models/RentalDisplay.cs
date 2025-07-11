95bc9b4774f2c64d6f1d5f10ff31a0837343ce6e
Cedvenx29 <81857441+Cedvenx29@users.noreply.github.com>
2025-07-12 06:51:48 +0800

    Add CarRentalSystem/Models/RentalDisplay.cs 1752274307952

---
+++ CarRentalSystem/Models/RentalDisplay.cs
@@ -0,0 +1,12 @@
+namespace CarRentalSystem.Models
+{
+    public class RentalDisplay
+    {
+        public int Id { get; set; }
+        public string CarDisplay { get; set; }
+        public string CustomerDisplay { get; set; }
+        public string RentalDate { get; set; }
+        public string ReturnDate { get; set; }
+        public string Price { get; set; }
+    }
+}
