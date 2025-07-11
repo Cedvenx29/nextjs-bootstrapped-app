15d79fc9df251eee0d84f862f70eed436da55ead
Cedvenx29 <81857441+Cedvenx29@users.noreply.github.com>
2025-07-12 06:51:50 +0800

    Add CarRentalSystem/Models/Customer.cs 1752274310340

---
+++ CarRentalSystem/Models/Customer.cs
@@ -0,0 +1,10 @@
+namespace CarRentalSystem.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string LicenseNo { get; set; }
+    }
+}
